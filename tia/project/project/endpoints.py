ac547cebe39afa8babecf47439b57db664882c44
JakubNvk <jakub.novak@gmail.com>
2018-05-15 10:48:15 +0200

    update: Endpoints
    Signed-off-by: JakubNvk <jakub.novak@gmail.com>

--- tia/project/project/endpoints.py
+++ tia/project/project/endpoints.py
@@ -3,6 +3,8 @@ from flask.json import jsonify, loads
 from flask_restful import Resource
 from flask.ext.login import current_user
 
+from sqlalchemy import and_
+
 from .data import db
 
 
@@ -110,8 +112,7 @@ class ExpeditionDetail(Resource):
         ''' Edit expedition info. '''
 
         try:
-            expedition = Expedition.query.filter(Expedition.id == id).one()
-            # if
+            expedition = Expedition.query.filter(and_(Expedition.id == id)).one()
             expedition_data = loads(request.data)
             expedition = expedition.update(**expedition_data)
 
@@ -126,7 +127,6 @@ class ExpeditionDetail(Resource):
 
         try:
             expedition = Expedition.query.filter(Expedition.id == id).one()
-            # if
             expedition.delete()
 
             return 200
@@ -137,15 +137,40 @@ class ExpeditionDetail(Resource):
 class ExpeditionMembership(Resource):
     def get(self, id):
         ''' Get expedition members. '''
+
         try:
-            pass
+            expedition = Expedition.query().filter(Expedition.id == id).one()
+
+            return jsonify(id=id, members=expedition.members)
         except Exception as e:
             abort(404)
 
     def put(self, id):
         ''' Add user to expedition. '''
-        pass
+
+        user = current_user
+        try:
+            expedition = Expedition.query().filter(and_(Expedition.id == id,
+                                                        Expedition.members.any(user))).one()
+            expedition.members.append(user)
+
+            return 200
+        except Exception as e:
+            abort(500)
+
 
     def delete(self, id):
         ''' Delete user from expedition. '''
-        pass
+        
+        user = current_user
+        try:
+            expedition = Expedition.query().filter(Expedition.id == id).one()
+            members = expedition.members
+
+            for i in range(len(members)):
+                if members[i].id == user.id:
+                    members.remove(members[i])
+
+            return 200
+        except Exception as e:
+            abort(500)
