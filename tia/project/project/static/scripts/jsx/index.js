dbabe5d05bd170cfcdead72c3f18e671c089ada9
JakubNvk <jakub.novak@gmail.com>
2018-05-18 00:27:33 +0200

    update: Navigation
    Signed-off-by: JakubNvk <jakub.novak@gmail.com>

--- tia/project/project/static/scripts/jsx/index.js
+++ tia/project/project/static/scripts/jsx/index.js
@@ -1,8 +1,2 @@
-import Hello from './Hello';
-import Navigation from './Navigation'
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-ReactDOM.render(<Hello/>, document.getElementById('reactEntry'));
-ReactDOM.render(<Navigation/>, document.getElementById('react'));
